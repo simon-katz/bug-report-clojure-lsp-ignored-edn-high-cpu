f90c05e24fc45135876ae687dadfe57bf82da115
Simon Katz <sk@nomistech.com>
2023-05-27 22:26:31 +0100

    Initial commit

---
+++ src/create_edn_files.clj
@@ -0,0 +1,27 @@
+(ns create-edn-files
+  (:require [clojure.pprint :as pp]))
+
+(def output-dir "edn-files")
+
+(defn create-edn-file [name n map-size]
+  (.mkdirs (java.io.File. output-dir))
+  (spit (str output-dir "/" name "-" n "-" map-size ".edn")
+        (with-out-str
+          (pp/pprint
+           (for [_ (range n)]
+             (apply sorted-map
+                    (apply concat
+                           (for [i (range map-size)]
+                             [(-> i (+ 97) char str keyword)
+                              (inc i)]))))))))
+
+(comment
+  (do
+    (create-edn-file "small" 5 4)
+    (create-edn-file "medium" 1500 4)
+    ;; Stackoverflow with big files:
+    #_(create-edn-file "big" 2000 4)
+    ;; Emacs beceomes unresponsive with very big files,
+    ;; even if lsp has been told to ignore them.
+    #_(create-edn-file "very-big" 10000 26)
+    ))
